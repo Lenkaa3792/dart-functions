08147be3772a86ec18c920104ca47644d26b2f9b
Lenkaa3792 <allanlenkaa@gmail.com>
2023-11-14 20:03:41 +0300

    loops and switch

---
+++ whileloop.dart
@@ -0,0 +1,9 @@
+// executes  a condition until its false
+void main() {
+  var a = 1;
+  var maxnum = 10;
+  while (a < maxnum) {
+    print(a); // prints untils its false
+    a = a + 1;// increases value of a after every iteration/loop/cycle
+  }
+}
